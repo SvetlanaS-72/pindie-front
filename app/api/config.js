1bdee985bb8c5e40bbeab1552e5c7f3cfb58d3e9
SvetlanaS-72 <146142217+SvetlanaS-72@users.noreply.github.com>
2024-05-29 21:18:27 +0300

    feat:added domen in URL front

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,5 @@
-export const BASE_URL = 'http://localhost:3001/api'
+export const BASE_URL = "https://student-svets.nomoredomainswork.ru/api"
+//export const BASE_URL =  "http://localhost:3001/api"                     
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
