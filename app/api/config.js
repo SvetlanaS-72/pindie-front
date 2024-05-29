521fe0576a929a586877c5321b56c62562e7ddd3
SvetlanaS-72 <146142217+SvetlanaS-72@users.noreply.github.com>
2024-05-29 23:26:26 +0300

    Финальное ревью

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,6 @@
-export const BASE_URL = 'http://localhost:3001/api'
+export const BASE_URL = 'https://student-svets.nomoredomainswork.ru/api'
+//export const BASE_URL = 'http://localhost:3001/api'                   
+                        
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
